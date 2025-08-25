88776ef93a55171387ab2d2d47abb35a58d86d10
Matheus Vasconcelos <163208992+Matheus-Vasco01@users.noreply.github.com>
2025-08-25 20:29:27 -0300

    Create cadeira_preta.py

---
+++ cadeira_preta.py
@@ -0,0 +1,5 @@
+O que é um cabeçalho em um arquivo Python, para que serve? O que é uma docstring e qual a sua utilidade?
+o cabecalho e a parte inicial de um arquivo py.
+
+a Docstring serve para documentar funcoes, classes e modulos, Fica logo após a declaração e serve para explicar o que o código faz.
+
